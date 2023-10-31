0967b64b9b4250de34284ab5b6e1eacd94305bbd
Utus Karta Sanggam <88187616+utusks001@users.noreply.github.com>
2023-10-31 18:57:57 +0700

    Update e_commerce_app.py

--- e_commerce_app.py
+++ e_commerce_app.py
@@ -30,7 +30,7 @@ import time
 from PIL import Image
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-from io import StringIO
+
 
 import warnings
 warnings.filterwarnings("ignore")
